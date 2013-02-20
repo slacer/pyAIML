6927ec91b027624ad7cfaf88d8b63b5f03b6d663
Spencer McIntyre <zeroSteiner@gmail.com>
2013-02-20 12:50:48 -0500

    Move the aiml modules into the aiml directory to form a package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'pyAIML',
+	version = '0.8.6',
+	description = 'PyAIML -- The Python AIML Interpreter',
+	author = 'Cort Stratton',
+	maintainer = 'Spencer McIntyre',
+	author_email = 'cort@users.sourceforge.net',
+	maintainer_email = '',
+	url = 'https://github.com/zeroSteiner/pyAIML',
+	packages = [ 'aiml' ],
+	)
